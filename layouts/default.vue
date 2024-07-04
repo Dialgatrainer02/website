e011e131b666f275b74519ba9631c210cb9d2415
Dialgatrainer02 <oliviabarker900@gmail.com>
2024-07-04 09:23:24 +0100

    basic layout with theme

---
+++ layouts/default.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="hero bg-base-200 min-h-screen">
+    <div class="hero-content text-center">
+      <div class="max-w-md">
+        <h1 class="text-5xl font-bold">
+          <slot name="title">
+
+          </slot>
+        </h1>
+        <p class="py-6">
+          <slot name="content">
+            
+          </slot>
+        </p>
+        <button class="btn btn-primary">
+          <slot name="button">
+
+          </slot>
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <NuxtLayout>
+        <template #title>
+            home
+        </template>
+        <template #content>
+            ooo
+        </template>
+        <template #button>
+            more testing
+        </template>
+   </NuxtLayout>
+</template>
