1e9c03f6a28a07d067f133f350bcd0a9f7fe8999
Dialgatrainer02 <oliviabarker900@gmail.com>
2024-09-04 15:38:30 +0100

    minor changes

--- pages/holidays.vue
+++ pages/holidays.vue
@@ -9,7 +9,19 @@
             <NuxtImg src="https://files.holidaycottages.co.uk/breconcottages%2Fguides%2F1621957833519-abergavenny+town.jpg"></NuxtImg>
           </p>
           <h2 class="text-lg"> south coast</h2>
-          <p> i went down to lymington too see my grandparents</p>
+          <p> i went down to lymington too see my grandparents. we had icecream </p>
+          <ul>
+            <li> go to cinema</li>
+            <NuxtLink to="https://lymingtoncommunity.com/">comunity centre</NuxtLink>
+            <iframe class="invisible" src="https://www.google.com/maps/place/Lymington/@50.7595633,-1.5438932,16.48z/data=!4m6!3m5!1s0x487380d551efa449:0xcd8cf49129a73d50!8m2!3d50.758531!4d-1.5419099!16zL20vMDIwNmhu?entry=ttu&g_ep=EgoyMDI0MDgyOC4wIKXMDSoASAFQAw%3D%3D" frameborder="0"></iframe>
+            <iframe class="invisible" src="https://www.google.com/maps?output=search&q=bournemouth+beach&fbs=AEQNm0Aa4sjWe7Rqy32pFwRj0UkWd8nbOJfsBGGB5IQQO6L3J3ppPdoHI1O-XvbXbpNjYYwWUVH6qTfR1Lpek5F-7GS5RQweTIdCTG69vaR2CWslmwuhrh1f3LHBOO-K838CxMhYEAjW-c5ZEesUaLgetDEA4kbgTftyF00IUVRwbgx6VAcU-b_zRCdMu2-swQD6v_h8u5wj&entry=mc&ictx=111"></iframe>
+            <NuxtLink class="btn btn-outline btn-primary" to="https://www.bournemouth.co.uk/things-to-do/beaches">bournemouth</NuxtLink>
+            <li>see grandparents</li>
+            <NuxtLink class="btn btn-primary"to="https://lymingtoncommunity.com/"> to go the market</NuxtLink>
+            <li>get more icecream</li>
+          </ul>
+          <NuxtImg src="https://www.toadhallcottages.co.uk/blog/wp-content/uploads/2023/04/lymington-cobbles.jpg"></NuxtImg>
+          <iframe class="invisible" src="https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUMcmljayBhc3RsZXJ5" frameborder="0"></iframe>
       </template>
       <template #button>
           <NuxtLink class="link" to="/">Home</NuxtLink>
